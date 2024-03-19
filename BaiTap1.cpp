10e87b11077c982d3928d92f74ef29cec8fceca7
nguyncutie09 <161151759+nguyncutie09@users.noreply.github.com>
2024-03-19 14:59:52 +0700

    Add files via upload

---
+++ BaiTap1.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <conio.h>
+void inBoi7(int n) {
+	// Kiểm tra n có phải là bội của 7 hay không
+	if (n % 7 == 0) {
+		// In ra n
+		printf("%d ", n);
+	}
+}
+
+int main() {
+	// Duyệt từ 10 đến 99
+	for (int i = 10; i <= 99; i++) {
+		// Gọi hàm inBoi7 với tham số i
+		inBoi7(i);
+	}
+	printf("\nLe Thanh Ky Nguyen");
+	_getch();
+	return 0;
+}
